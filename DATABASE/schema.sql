be8caa804a9fe283836c6fa2186a84bf645370a4
Anna Liu <aliu426@uwo.ca>
2025-01-14 22:47:51 -0500

    Update schema.sql

--- DATABASE/schema.sql
+++ DATABASE/schema.sql
@@ -3,17 +3,17 @@ CREATE DATABASE ues_stationbook;
 USE ues_stationbook;
 
 CREATE TABLE students (
-	student_id 			INT(10) NOT NULL PRIMARY KEY,
-    email 				VARCHAR(20) NOT NULL,
+	student_id 			INT(10) UNIQUE NOT NULL PRIMARY KEY,
+    email 				VARCHAR(20) UNIQUE NOT NULL,
     first_name			VARCHAR(20) NOT NULL,
     last_name			VARCHAR(20) NOT NULL,
     nickname			VARCHAR(20),
-    password_hash		VARCHAR(50) NOT NULL,
+    password_hash		VARCHAR(255) NOT NULL,
     points				INT(30),
-    email_verified		BOOL NOT NULL,
-    is_active 			BOOL NOT NULL,
-    is_admin			BOOL NOT NULL,
-    created_at			TIMESTAMP,
-    updated_at			TIMESTAMP
+    email_verified		BOOLEAN DEFAULT false,
+    is_active 			BOOLEAN DEFAULT true,
+    is_admin			BOOLEAN DEFAULT false,
+    created_at			TIMESTAMP DEFAULT current_timestamp,
+    updated_at			TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp
 	);
     
